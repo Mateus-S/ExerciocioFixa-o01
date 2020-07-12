ea4f5e059509f6a72ae2351541004c28760297fc
Mateus Santos <mateus.gcr.tog@hotmail.com>
2020-07-11 21:32:42 -0300

    Inicio do Exercicio de Fixação sobre Enums e Entities

---
+++ Exercio01Fixacao/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercio01Fixacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
